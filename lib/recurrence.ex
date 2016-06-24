84b81403db32a5065cd52bc70acc6c3b13929814
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-06-24 17:25:18 +0200

    Implement simple month & week recurrence

--- lib/recurrence.ex
+++ lib/recurrence.ex
@@ -1,2 +1,45 @@
 defmodule Recurrence do
+  def compute(options) do
+    starts = Keyword.fetch!(options, :starts)
+    until = Keyword.get(options, :until)
+    every = Keyword.fetch!(options, :every)
+    interval = Keyword.fetch!(options, :interval)
+    
+    compute_from = Keyword.fetch!(options, :from)
+    compute_to = Keyword.fetch!(options, :to)
+
+    recur(starts, until, every, interval, compute_from, compute_to, 0, [])
+  end
+
+  def shift!(date, parameters) do
+    case Timex.shift(date, parameters) do
+      {:error, _term} ->
+        # TODO: propagate term up there
+        raise "Time shift failed with error"
+      output ->
+        output
+    end
+  end
+
+  # TODO: verify how we could replace this by Timex.Interval
+  # TODO: create an enumerator for long series support
+  def recur(starts, until, every, interval, compute_from, compute_to, index, results) do
+    # TODO: handle error (e.g. wrong interval type)
+    date = shift!(starts, [{interval, index * every}])
+    cond do
+      # Have we reached the end of the observation window?
+      # TODO: use after?
+      Timex.Date.compare(date, compute_to) > 0 ->
+        results # stop recurrence
+      # Have we reached the optional upper bound of the recurrence?
+      until && (Timex.Date.compare(date, until) > 0) ->
+        results # stop recurrence
+      # Are we inside the observation window? If so keep the record
+      Timex.Date.compare(date, compute_from) >= 0 ->
+        [date | recur(starts, until, every, interval, compute_from, compute_to, index + 1, results)]
+      # Otherwise just skip a beat without adding the record
+      true ->
+        recur(starts, until, every, interval, compute_from, compute_to, index + 1, results)
+    end
+  end
 end
--- test/recurrence_test.exs
+++ test/recurrence_test.exs
@@ -1,8 +1,77 @@
 defmodule RecurrenceTest do
   use ExUnit.Case
   doctest Recurrence
+  use Timex
+  
+  test "one month iteration with end date" do
+    dates = Recurrence.compute(
+      # recurrence setup
+      every: 1,
+      interval: :months,
+      starts: %Date{year: 2012, month: 1, day: 31},
+      until: %Date{year: 2012, month: 4, day: 1},
 
-  test "the truth" do
-    assert 1 + 1 == 2
+      # computation window (TODO: split)
+      from: %Date{year: 2012, month: 2, day: 1},
+      to: %Date{year: 2012, month: 4, day: 1}
+    )
+    assert dates == [
+      %Date{year: 2012, month: 2, day: 29},
+      %Date{year: 2012, month: 3, day: 31},
+    ]
   end
+
+  test "one month iteration without end date" do
+    dates = Recurrence.compute(
+      # recurrence setup
+      every: 1,
+      interval: :months,
+      starts: %Date{year: 2012, month: 1, day: 31},
+
+      # computation window (TODO: split)
+      from: %Date{year: 2012, month: 2, day: 1},
+      to: %Date{year: 2012, month: 6, day: 1}
+    )
+    assert dates == [
+      %Date{year: 2012, month: 2, day: 29},
+      %Date{year: 2012, month: 3, day: 31},
+      %Date{year: 2012, month: 4, day: 30},
+      %Date{year: 2012, month: 5, day: 31},
+    ]
+  end
+  
+  test "two month iteration with end date" do
+    dates = Recurrence.compute(
+      # recurrence setup
+      every: 2,
+      interval: :months,
+      starts: %Date{year: 2011, month: 12, day: 31},
+
+      # computation window (TODO: split)
+      from: %Date{year: 2012, month: 1, day: 25},
+      to: %Date{year: 2012, month: 6, day: 1}
+    )
+    assert dates == [
+      %Date{year: 2012, month: 2, day: 29},
+      %Date{year: 2012, month: 4, day: 30},
+    ]
+  end
+  
+  test "one week iteration with end date" do
+    dates = Recurrence.compute(
+      # recurrence setup
+      every: 1,
+      interval: :weeks,
+      starts: %Date{year: 2015, month: 12, day: 31}, # thursday
+
+      # computation window (TODO: split)
+      from: %Date{year: 2016, month: 6, day: 24},
+      to: %Date{year: 2016, month: 7, day: 1}
+    )
+    assert dates == [
+      %Date{year: 2016, month: 6, day: 30},
+    ]
+  end
+  
+  test "incorrect interval"
 end
